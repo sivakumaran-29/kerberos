d9279409b24c9c40d630ed962421a2e98932a40f
sivakumaran-29 <sivakumaran239@example.com>
2026-09-05 10:50:56 +0530

    Fix: Move Vercel config to root to ensure detection

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ git clone https://github.com/flutter/flutter.git -b stable
 # Add flutter to path
 export PATH="$PATH:`pwd`/flutter/bin"
 
+cd client
+
 echo ">>> Injecting Environment Variables..."
 echo "SUPABASE_URL=$SUPABASE_URL" > .env
 echo "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" >> .env
